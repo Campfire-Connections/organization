3d431d6c7cadbdbdc398429614c9d0aedd1f56d7
David Durost <david.durost@gmail.com>
2025-11-19 19:42:25 -0500

    Test organization hierarchy helper behavior using shared fixtures

--- tests.py
+++ tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+from core.tests import BaseDomainTestCase
+from organization.models import Organization
+from faction.models import Faction
+
+
+class OrganizationModelTests(BaseDomainTestCase):
+    def test_total_factions_counts_descendants(self):
+        sub_org = Organization.objects.create(
+            name="Foothills Outpost",
+            abbreviation="FO",
+            parent=self.organization,
+            max_depth=5,
+        )
+        Faction.objects.create(name="Foothills Crew", organization=sub_org)
+
+        self.assertEqual(self.organization.get_total_factions_count(), 2)
+
+    def test_root_lookup_traverses_parents(self):
+        self.assertEqual(self.organization.get_root_organization(), self.parent_org)
