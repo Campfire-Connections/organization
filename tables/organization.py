bfc492b6000e7f1f5180efcfaf9eb0df336599a3
David Durost <david.durost@gmail.com>
2025-11-22 10:01:33 -0500

    Convert organization table to BaseTable actions

--- tables/organization.py
+++ tables/organization.py
@@ -2,18 +2,26 @@
 
 import django_tables2 as tables
 
+from core.tables.base import BaseTable
 from ..models.organization import Organization
 
 
-class OrganizationTable(tables.Table):
-    controls = tables.TemplateColumn(
-        template_name="organization/partials/controls.html",
-        verbose_name="Actions",
-        orderable=False,
-    )
+class OrganizationTable(BaseTable):
+    available_actions = ["show", "edit", "delete"]
+    url_namespace = ""  # explicit names below
 
     class Meta:
         model = Organization
         template_name = "django_tables2/bootstrap4.html"
         fields = ("name", "description", "abbreviation", "max_depth", "parent")
         attrs = {"class": "table table-striped table-bordered"}
+
+    urls = {
+        "add": {"name": "organization_new", "kwargs": {}},
+        "show": {"name": "organization_show", "kwargs": {"organization_slug": "slug"}},
+        "edit": {"name": "organization_edit", "kwargs": {"organization_slug": "slug"}},
+        "delete": {
+            "name": "organization_delete",
+            "kwargs": {"organization_slug": "slug"},
+        },
+    }
