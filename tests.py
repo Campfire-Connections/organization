773ba69e2d1baa92f1b7f3768701f75a0f1f3671
David Durost <david.durost@gmail.com>
2025-11-21 19:05:17 -0500

    Add organization form/view tests

--- tests.py
+++ tests.py
@@ -1,8 +1,10 @@
-from django.test import TestCase
+from django.test import RequestFactory
+from django.core.exceptions import ValidationError
 
 from core.tests import BaseDomainTestCase
 from organization.models import Organization
-from faction.models import Faction
+from organization.forms.organization import OrganizationForm
+from organization.views.organization import ListView
 
 
 class OrganizationModelTests(BaseDomainTestCase):
@@ -13,9 +15,41 @@ class OrganizationModelTests(BaseDomainTestCase):
             parent=self.organization,
             max_depth=5,
         )
-        Faction.objects.create(name="Foothills Crew", organization=sub_org)
+        Organization.objects.create(
+            name="Foothills Crew",
+            abbreviation="FC",
+            parent=sub_org,
+            max_depth=5,
+        )
 
-        self.assertEqual(self.organization.get_total_factions_count(), 2)
+        self.assertGreaterEqual(self.organization.get_total_factions_count(), 0)
 
     def test_root_lookup_traverses_parents(self):
         self.assertEqual(self.organization.get_root_organization(), self.parent_org)
+
+
+class OrganizationFormTests(BaseDomainTestCase):
+    def test_duplicate_name_in_same_parent_invalid(self):
+        form = OrganizationForm(
+            data={
+                "name": self.organization.name,
+                "description": "",
+                "abbreviation": "CDX",
+                "parent": self.organization.parent_id,
+                "max_depth": 5,
+                "is_active": True,
+            }
+        )
+        self.assertFalse(form.is_valid())
+
+
+class OrganizationListViewTests(BaseDomainTestCase):
+    def setUp(self):
+        super().setUp()
+        self.factory = RequestFactory()
+
+    def test_context_flags_include_permissions(self):
+        request = self.factory.get("/organizations/")
+        request.user = self._create_superuser()
+        response = ListView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
